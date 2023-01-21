da706d2d6abb492c61b8de55c469185e780f7912
LJH-148 <quando148@gmail.com>
2023-01-21 20:45:35 +0900

    first commit

---
+++ class2.dart
@@ -0,0 +1,43 @@
+void main() {
+  // positional parameter. 포지셔널 파라미터. 외부에서 입력.
+  Idol blackPink = Idol('블랙핑크', ['지수', '제니', '리사', '로제']);
+
+  print(blackPink.name); // 변수 출력.
+  print(blackPink.members);
+  blackPink.sayHello(); // 함수 출력.
+  blackPink.introduce();
+
+  Idol bts = Idol('BTS', ['RM', '진', '슈가', '제이홉', '지민', '뷔', '정국']);
+
+  print(bts.name);
+  print(bts.members);
+  bts.sayHello();
+  bts.introduce();
+}
+
+// Idol class
+// name (이름) - 변수
+// members (멤버들) - 변수
+// sayHello (인사) - 함수
+// introduce (멤버소개) - 함수
+//
+// constructor (생성자)
+
+class Idol {
+  String name; // = '블랙핑크'; X
+  List<String> members; // = ['지수', '제니', '리사', '로제']; X
+
+// constructor - 컨스트럭터 - 생성자
+// positional parameter 생성. 외부에서 입력.
+  Idol(String name, List<String> members)
+      : this.name = name,
+        this.members = members;
+
+  void sayHello() {
+    print('안녕하세요 ${this.name}입니다.');
+  }
+
+  void introduce() {
+    print('저희 멤버는 ${this.members}가 있습니다.');
+  }
+}
