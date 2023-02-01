715ef8488406f472b0dafa5f95a3ce4c07007b63
LJH-148 <quando148@gmail.com>
2023-02-01 20:25:53 +0900

    reduce2

---
+++ reduce2.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<String> words = ['오늘은 ', '2023년 ', '2월1일 ', '수요일 ', '입니다.'];
+
+  final result = words.reduce((prev, next) => prev + next);
+
+  print(result);
+  // 오늘은 2023년 2월1일 수요일 입니다.
+
+  // 리듀스가 실행할 수 없는 경우의 수가 있다.
+  // 리듀스로 반환되는 타입이 리스트 리턴타입과 같아야 한다.
+  final result2 = words.reduce((prev, next) => prev.length + next.length);
+
+  print(result2);
+  // error - String 타입으로 받아야하는데 length가 int값.
+}
