b0caf40ffabadd5fbb0f87ea206e019e2b43d9bb
LJH-148 <quando148@gmail.com>
2023-02-01 20:41:34 +0900

    reduce2

--- reduce2.dart
+++ reduce2.dart
@@ -1,10 +1,10 @@
 void main() {
-  List<String> words = ['오늘은 ', '2023년 ', '2월1일 ', '수요일 ', '입니다.'];
+  List<String> words = ['오늘은 ', '수요일 ', '입니다.'];
 
   final result = words.reduce((prev, next) => prev + next);
 
   print(result);
-  // 오늘은 2023년 2월1일 수요일 입니다.
+  // 오늘은 수요일 입니다.
 
   // 리듀스가 실행할 수 없는 경우의 수가 있다.
   // 리듀스로 반환되는 타입이 리스트 리턴타입과 같아야 한다.
